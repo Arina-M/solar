ef9a20494010d3b6dc111e746eea063e6a4997c0
Arina-M <arishka.mail@gmail.com>
2026-04-23 12:53:40 +0300

    solar checklist section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,3 +137,30 @@ calculateBtn.addEventListener("click", () => {
 
 updateUI();
 calculate(); // стартовые значения как в макете
+
+
+
+
+
+
+
+
+
+
+
+//аккордеон почему солар
+const items = document.querySelectorAll(".accordion-item");
+
+items.forEach(item => {
+    const header = item.querySelector(".accordion-header");
+
+    header.addEventListener("click", () => {
+        const isActive = item.classList.contains("active");
+
+        items.forEach(i => i.classList.remove("active"));
+
+        if (!isActive) {
+            item.classList.add("active");
+        }
+    });
+});
